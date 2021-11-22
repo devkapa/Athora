0fd8373b184835455b1a953d4177502941ca4853
BuildTools <unconfigured@null.spigotmc.org>
2021-11-22 14:39:06 +1100

    Add logic

--- AthoraLogic.java
+++ AthoraLogic.java
@@ -1,50 +1,11 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class AthoraLogic {
 
-    public static ArrayList<AthoraScene> athoraScenes = new ArrayList<>();
-
-    public static void run() throws IOException, ParseException {
-
-        String jsonPath = "AthoraScenes.json";
-
-        JSONParser parser = new JSONParser();
-
-        JSONArray scenes = (JSONArray) parser.parse(new FileReader(jsonPath));
-
-        for (Object scene : scenes) {
-
-            JSONObject s = (JSONObject) scene;
-
-            ArrayList<Long> directions = new ArrayList<>();
-            directions.add((long) s.get("north"));
-            directions.add((long) s.get("east"));
-            directions.add((long) s.get("south"));
-            directions.add((long) s.get("west"));
-
-            AthoraScene selectedScene = new AthoraScene(
-                    (long) s.get("id"),
-                    (String) s.get("name"),
-                    directions,
-                    (String) s.get("setting")
-            );
-
-            athoraScenes.add(selectedScene);
-
-        }
-    }
-
     public static void main(String[] args) throws IOException, ParseException {
-        run();
-
-        System.out.println(athoraScenes);
+        AthoraScene.InitiateScenes();
     }
 
 }
--- AthoraScene.java
+++ AthoraScene.java
@@ -1,3 +1,10 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AthoraScene {
@@ -16,6 +23,54 @@ public class AthoraScene {
 
     public long getId(){
         return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList<Long> getDirections() {
+        return directions;
+    }
+
+    public String getSetting(){
+        return setting;
+    }
+
+    public static ArrayList<AthoraScene> athoraScenes = new ArrayList<>();
+    public static AthoraScene currentScene;
+
+    public static void InitiateScenes() throws IOException, ParseException {
+
+        String jsonPath = "AthoraScenes.json";
+
+        JSONParser parser = new JSONParser();
+
+        JSONArray scenes = (JSONArray) parser.parse(new FileReader(jsonPath));
+
+        for (Object scene : scenes) {
+
+            JSONObject s = (JSONObject) scene;
+
+            ArrayList<Long> directions = new ArrayList<>();
+            directions.add((long) s.get("north"));
+            directions.add((long) s.get("east"));
+            directions.add((long) s.get("south"));
+            directions.add((long) s.get("west"));
+
+            AthoraScene selectedScene = new AthoraScene(
+                    (long) s.get("id"),
+                    (String) s.get("name"),
+                    directions,
+                    (String) s.get("setting")
+            );
+
+            athoraScenes.add(selectedScene);
+
+        }
+
+        currentScene = athoraScenes.get(0);
+
     }
 
 }
