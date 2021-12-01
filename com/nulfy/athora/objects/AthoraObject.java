ed282617f027f96ead778922ff997bc1f1ed0678
Dev Kapadia <dev@kapadia.biz>
2021-12-01 12:16:41 +1100

    Updated object logic (Still W.I.P.)

--- com/nulfy/athora/objects/AthoraObject.java
+++ com/nulfy/athora/objects/AthoraObject.java
@@ -5,11 +5,13 @@ public class AthoraObject {
     private final long id;
     private final String name;
     private final String type;
+    private final boolean accessible;
 
-    public AthoraObject(long id, String name, String type) {
+    public AthoraObject(long id, String name, String type, boolean accessible) {
         this.id = id;
         this.name = name;
         this.type = type;
+        this.accessible = accessible;
     }
 
     public long getId() {
@@ -24,4 +26,7 @@ public class AthoraObject {
         return type;
     }
 
+    public boolean isAccessible() {
+        return accessible;
+    }
 }
--- com/nulfy/athora/objects/AthoraWeapon.java
+++ com/nulfy/athora/objects/AthoraWeapon.java
@@ -2,15 +2,19 @@ package com.nulfy.athora.objects;
 
 public class AthoraWeapon extends AthoraObject {
 
-    private final long damage;
+    private final int damage;
 
-    public AthoraWeapon(long id, String name, String type, long damage) {
-        super(id, name, type);
+    public AthoraWeapon(long id, String name, String type, boolean accessible, int damage) {
+        super(id, name, type, accessible);
         this.damage = damage;
     }
 
-    public long getDamage() {
+    public int getDamage() {
         return damage;
+    }
+
+    public void attack(AthoraObstacle enemy, AthoraWeapon weapon) {
+        enemy.changeHealth(weapon.getDamage());
     }
 
 }
